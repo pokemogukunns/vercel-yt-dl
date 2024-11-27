11b1789f5208e88035ebbc57ea287a87de9c345f
pokemogukunns <nattyannkunndesu@icloud.com>
2024-11-27 13:49:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -280,6 +280,10 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
